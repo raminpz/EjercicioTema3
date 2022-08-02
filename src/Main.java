e88dd04271545b9536ceb543ec8ef700cbfc28f4
raminpz <raminp.1012@gmail.com>
2022-08-02 10:26:42 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] nombres = {"Ramiro","Emily","Carolina","Dayana"};
+        String s = String.join(" ", nombres);
+        System.out.println(s);
+    }
+}
